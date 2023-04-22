586dd1ed82ab58245dd4f5f8896473f3d0565f88
Julien <julien.esnau@gmail.com>
2023-04-23 01:14:31 +0200

    [200~feat: Center loading icon in MovieCard cover and adjust its size

--- src/pages/header/Header.tsx
+++ src/pages/header/Header.tsx
@@ -15,11 +15,19 @@ const Header: React.FC<HeaderProps> = ({ onSearch }) => {
   return (
     <header className="w-full flex flex-col items-center justify-center">
       <h1
-        className="text-8xl font-bold text-red-600 mb-4 text-center pb-8"
+        className="text-8xl font-bold text-red-600 mb-2 text-center pb-4"
         style={{ fontFamily: 'Bebas Neue' }}
       >
         Movie finder
       </h1>
+      <div className="mt-4 mb-4 px-4 max-w-4xl mx-auto text-center text-white">
+        <h1 className="text-4xl font-bold">
+          Découvrez et discutez des films et séries du monde entier
+        </h1>
+        <p className="text-2xl mt-4 pb-10">
+          Recherchez un film ou une série et discutez avec le monde entier
+        </p>
+      </div>
       <form
         onSubmit={handleSubmit}
         className="w-full md:w-1/2 flex items-center justify-center"
