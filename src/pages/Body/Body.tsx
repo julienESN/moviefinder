fcedcdfb693aab17108953745d9348a97ad761c5
Julien <julien.esnau@gmail.com>
2023-04-23 02:57:40 +0200

    feat: Improve error message and loading UI
    - Center error message responsively in the middle of the page
    - Center loading icon and add a loading indicator for background video

--- src/pages/Body/Body.tsx
+++ src/pages/Body/Body.tsx
@@ -2,9 +2,8 @@ import React, { useState } from 'react';
 import Header from '../header/Header';
 import BackgroundVideo from '@/Backgroundvideo/Backgroundvideo';
 import Image from 'next/image';
-import MovieCard from '../MovieCard/MovieCard'; // Import du composant MovieCard
+import MovieCard from '../MovieCard/MovieCard';
 
-// Définition de l'interface pour les données des films
 interface MovieData {
   Title: string;
   Year: string;
@@ -16,66 +15,75 @@ interface MovieData {
 }
 
 const Body: React.FC = () => {
-  // États pour les données des films, le chargement, le titre du film et les messages d'erreur
   const [movies, setMovies] = useState<MovieData[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [movieTitle, setMovieTitle] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
-  // Fonction pour rechercher les films
   const searchMovies = async (title: string) => {
-    setIsLoading(true); // Affiche le chargement
+    setIsLoading(true);
     setMovieTitle(title);
-    setErrorMessage(''); // Réinitialise le message d'erreur
+    setErrorMessage('');
 
     try {
-      // Requête API pour rechercher les films
       const response = await fetch(
         `/api/searchMovie?title=${encodeURIComponent(title)}`
       );
       const data: MovieData = await response.json();
 
-      // Gère les erreurs renvoyées par l'API
       if (data.Error) {
         setErrorMessage(data.Error);
       } else {
         setMovies([data]);
       }
     } catch (error) {
-      // Gère les erreurs de requête
       console.error('Failed to fetch movies', error);
       setErrorMessage('Une erreur est survenue lors de la recherche de films.');
     } finally {
-      setIsLoading(false); // Cache le chargement
+      setIsLoading(false);
     }
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500">
       <Header onSearch={searchMovies} />
       <div className="w-full mt-10 flex flex-col md:flex-row h-full">
         <div className="w-full md:w-1/2 h-full">
           {isLoading ? (
-            // Affiche l'icône de chargement si isLoading est vrai
-            <Image
-              src="/assets/Rolling-0.9s-204px.svg"
-              alt="Icône de chargement"
-              width={204}
-              height={204}
-              className="text-xl font-semibold mx-auto block"
-            />
-          ) : errorMessage ? (
-            // Affiche le message d'erreur s'il existe
-            <p className="text-red-500 text-center">{errorMessage}</p>
+            <div className="relative w-full h-[400px] md:h-[px]">
+              <Image
+                src="/assets/Rolling-0.9s-204px.svg"
+                alt="Icône de chargement"
+                width={100}
+                height={100}
+                className="text-xl font-semibold mx-auto block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+              />
+            </div>
           ) : (
-            // Affiche les cartes de films en utilisant le composant MovieCard
             movies.map((movie, index) => (
               <MovieCard key={index} movie={movie} />
             ))
           )}
         </div>
+        {errorMessage ? (
+          <div className="w-full flex justify-center">
+            <h1 className="text-red-500">{errorMessage}</h1>
+          </div>
+        ) : null}
         <div className="w-full md:w-1/2 h-full">
-          <BackgroundVideo movieTitle={movieTitle} />
+          {isLoading ? (
+            <div className="relative w-full h-[400px] md:h-[px]">
+              <Image
+                src="/assets/Rolling-0.9s-204px.svg"
+                alt="Icône de chargement"
+                width={100}
+                height={100}
+                className="text-xl font-semibold mx-auto block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+              />
+            </div>
+          ) : (
+            <BackgroundVideo movieTitle={movieTitle} />
+          )}
         </div>
       </div>
     </div>
